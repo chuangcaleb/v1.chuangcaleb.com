5f3133b5cb56f96364dab7dbdee9de0bf2cd9fa6
chuangcaleb <balechuang@gmail.com>
2023-02-08 18:41:13 +0800

    ui: setup prose dark mode colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBackground: "#1c1f2b",
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-invert-body": theme("colors.zinc[200]"),
+            "--tw-prose-invert-headings": theme("colors.zinc[200]"),
+            "--tw-prose-invert-lead": theme("colors.zinc[200]"),
+            "--tw-prose-invert-links": theme("colors.zinc[200]"),
+            "--tw-prose-invert-code": theme("colors.zinc[200]"),
+          },
+        },
+      }),
+    },
   },
   plugins: [require("@tailwindcss/typography")],
+  // darkMode: "class",
 };
