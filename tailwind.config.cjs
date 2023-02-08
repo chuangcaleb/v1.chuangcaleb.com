6c01c459a30a31caa2b0d2321d4cea6746f9e27c
chuangcaleb <balechuang@gmail.com>
2023-02-09 00:36:46 +0800

    ui: set light/dark mode colour palette config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,21 +4,62 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        darkBackground: "#1c1f2b",
+        invert: { bg: "#1c1f2b" },
       },
       typography: ({ theme }) => ({
         DEFAULT: {
           css: {
-            "--tw-prose-invert-body": theme("colors.zinc[200]"),
-            "--tw-prose-invert-headings": theme("colors.zinc[200]"),
-            "--tw-prose-invert-lead": theme("colors.zinc[200]"),
-            "--tw-prose-invert-links": theme("colors.zinc[200]"),
-            "--tw-prose-invert-code": theme("colors.zinc[200]"),
+            code: {
+              "&::before": {
+                content: "none !important",
+              },
+              "&::after": {
+                content: "none !important",
+              },
+            },
+          },
+        },
+        zinc: {
+          css: {
+            "--tw-prose-body": theme("colors.zinc[600]"),
+            "--tw-prose-headings": theme("colors.zinc[700]"),
+            "--tw-prose-lead": theme("colors.zinc[600]"),
+            "--tw-prose-links": theme("colors.zinc[600]"),
+            "--tw-prose-code": theme("colors.zinc[600]"),
+            // "--tw-prose-bold": theme("colors.pink[900]"),
+            // "--tw-prose-counters": theme("colors.pink[600]"),
+            // "--tw-prose-bullets": theme("colors.pink[400]"),
+            // "--tw-prose-hr": theme("colors.pink[300]"),
+            // "--tw-prose-quotes": theme("colors.pink[900]"),
+            // "--tw-prose-quote-borders": theme("colors.pink[300]"),
+            // "--tw-prose-captions": theme("colors.pink[700]"),
+            // "--tw-prose-pre-code": theme("colors.pink[100]"),
+            // "--tw-prose-pre-bg": theme("colors.pink[900]"),
+            // "--tw-prose-th-borders": theme("colors.pink[300]"),
+            // "--tw-prose-td-borders": theme("colors.pink[200]"),
+
+            // * Invert
+            "--tw-prose-invert-body": theme("colors.zinc[300]"),
+            "--tw-prose-invert-headings": theme("colors.zinc[300]"),
+            "--tw-prose-invert-lead": theme("colors.zinc[300]"),
+            "--tw-prose-invert-links": theme("colors.zinc[300]"),
+            "--tw-prose-invert-code": theme("colors.zinc[300]"),
+            // "--tw-prose-invert-bold": theme("colors.white"),
+            // "--tw-prose-invert-counters": theme("colors.pink[400]"),
+            // "--tw-prose-invert-bullets": theme("colors.pink[600]"),
+            // "--tw-prose-invert-hr": theme("colors.pink[700]"),
+            // "--tw-prose-invert-quotes": theme("colors.pink[100]"),
+            // "--tw-prose-invert-quote-borders": theme("colors.pink[700]"),
+            // "--tw-prose-invert-captions": theme("colors.pink[400]"),
+            // "--tw-prose-invert-pre-code": theme("colors.pink[300]"),
+            // "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+            // "--tw-prose-invert-th-borders": theme("colors.pink[600]"),
+            // "--tw-prose-invert-td-borders": theme("colors.pink[700]"),
           },
         },
       }),
     },
   },
   plugins: [require("@tailwindcss/typography")],
-  // darkMode: "class",
+  darkMode: "class",
 };
