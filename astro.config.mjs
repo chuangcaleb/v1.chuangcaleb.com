3ea4960931a6f4b1ac59f5d606df07cc4764d76b
chuangcaleb <balechuang@gmail.com>
2023-02-15 18:47:04 +0800

    ci: setup astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), compress()]
 });
