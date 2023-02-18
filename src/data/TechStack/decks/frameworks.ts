bf697c4b1e963b2e4b62e9661c634b84ccdbf44c
chuangcaleb <balechuang@gmail.com>
2023-02-18 12:12:55 +0800

    ui: invert card logos

--- src/data/TechStack/decks/frameworks.ts
+++ src/data/TechStack/decks/frameworks.ts
@@ -6,7 +6,7 @@ const frameworks = {
   React: { iconBgColor: "responsive", icon: "logos:react" },
   Astro: { iconBgColor: "black", icon: "astro" },
   CRA: { iconBgColor: "responsive", icon: "logos:create-react-app" },
-  Next: { iconBgColor: "white", icon: "logos:nextjs-icon" },
+  Next: { iconBgColor: "responsive", icon: "logos:nextjs-icon", invert: true },
   Django: { icon: "logos:django-icon" },
 } satisfies TechDeck;
 
--- src/data/TechStack/types.ts
+++ src/data/TechStack/types.ts
@@ -2,6 +2,7 @@ export type TechCard = {
   icon: string;
   iconBgColor?: string;
   iconColor?: string;
+  invert?: boolean;
 };
 
 export type TechDeck = { [key: string]: TechCard };
