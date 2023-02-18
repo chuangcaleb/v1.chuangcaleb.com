190766f9ea67b6f114504c2cd541fc562c771e3f
chuangcaleb <balechuang@gmail.com>
2023-02-18 12:38:03 +0800

    data: create stats data structure

--- src/data/TechStack/decks/languages.ts
+++ src/data/TechStack/decks/languages.ts
@@ -1,8 +1,14 @@
 import type { TechDeck } from "../types";
 
 const languages = {
-  Javascript: { icon: { name: "logos:javascript" } },
-  Typescript: { icon: { name: "logos:typescript-icon" } },
+  Javascript: {
+    icon: { name: "logos:javascript" },
+    stats: { pro: 2, exp: 2, frs: 5, int: 5 },
+  },
+  Typescript: {
+    icon: { name: "logos:typescript-icon" },
+    stats: { pro: 3, exp: 3, frs: 5, int: 5 },
+  },
   // Python: { color: "sad" },
 } satisfies TechDeck;
 
--- src/data/TechStack/types.ts
+++ src/data/TechStack/types.ts
@@ -5,8 +5,18 @@ export type TechCardIcon = {
   invert?: boolean;
 };
 
+type Scale = 0 | 1 | 2 | 3 | 4 | 5;
+
+export type TechCardStats = {
+  pro?: Scale;
+  exp?: Scale;
+  frs?: Scale;
+  int?: Scale;
+};
+
 export type TechCard = {
   icon: TechCardIcon;
+  stats?: TechCardStats;
 };
 
 export type TechDeck = { [key: string]: TechCard };
