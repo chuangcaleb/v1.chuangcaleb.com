e63a4c9831cadc9491b305ceedb5ab809b45cc2d
chuangcaleb <balechuang@gmail.com>
2023-04-15 14:51:47 +0800

    ui: trip daisyui and typography crutches

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
-const colors = require("tailwindcss/colors");
+// const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("@tailwindcss/typography")],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
@@ -19,13 +19,13 @@ module.exports = {
           css: {
             "max-width": "inherit",
             // This is configured to daisyUI's `secondary-content` in hsl()
-            "--bc": "230 100% 93%",
+            // "--bc": "230 100% 93%",
           },
         },
         dark: {
           css: {
             "max-width": "inherit",
-            "--bc": "230 100% 97%",
+            // "--bc": "230 100% 97%",
           },
         },
         DEFAULT: {
@@ -36,39 +36,39 @@ module.exports = {
       }),
     },
   },
-  daisyui: {
-    logs: false,
-    themes: [
-      {
-        light: {
-          primary: "#7ea8a0",
-          secondary: "#484f70",
-          accent: "#EC9192",
-          neutral: colors.zinc[200],
-          "base-100": colors.zinc[50],
-          info: "#7dd3fc",
-          success: "#4ade80",
-          warning: "#fde047",
-          error: "#ef4444",
-        },
-        dark: {
-          primary: "#2563eb",
-          // primary: "#1143b3",
-          secondary: "#3e4460",
-          // secondary: "#484f70",
-          accent: "#b38111",
-          // neutral: "#202332",
-          neutral: "#161822",
-          "base-100": "#262a3b",
-          // "base-100": "#343951",
-          info: "#7dd3fc",
-          success: "#4ade80",
-          warning: "#fde047",
-          error: "#ef4444",
-        },
-      },
-    ],
-  },
+  // daisyui: {
+  //   logs: false,
+  //   themes: [
+  //     {
+  //       light: {
+  //         primary: "#7ea8a0",
+  //         secondary: "#484f70",
+  //         accent: "#EC9192",
+  //         neutral: colors.zinc[200],
+  //         "base-100": colors.zinc[50],
+  //         info: "#7dd3fc",
+  //         success: "#4ade80",
+  //         warning: "#fde047",
+  //         error: "#ef4444",
+  //       },
+  //       dark: {
+  //         primary: "#2563eb",
+  //         // primary: "#1143b3",
+  //         secondary: "#3e4460",
+  //         // secondary: "#484f70",
+  //         accent: "#b38111",
+  //         // neutral: "#202332",
+  //         neutral: "#161822",
+  //         "base-100": "#262a3b",
+  //         // "base-100": "#343951",
+  //         info: "#7dd3fc",
+  //         success: "#4ade80",
+  //         warning: "#fde047",
+  //         error: "#ef4444",
+  //       },
+  //     },
+  //   ],
+  // },
 
   // darkMode: "class",
 };
