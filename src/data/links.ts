a104b85160e0e12d612b43bae45b1134c5390cf9
chuangcaleb <balechuang@gmail.com>
2023-04-18 19:20:17 +0800

    ui: temporarily update hero CTA buttons to filled
    refac: extract constant social links

---
+++ src/data/links.ts
@@ -0,0 +1,4 @@
+export const LINK_LINKEDIN = "https://www.linkedin.com/in/chuangcaleb/";
+export const LINK_GITHUB = "https://github.com/chuangcaleb";
+export const LINK_CV =
+  "https://docs.google.com/document/d/1qohsYe0w-Mq5yqIdNaHPuOHvs2lYvnGySULgR2Baqwk/edit?usp=sharing";
