c4a0364f706ce55b13afc25643445fa69542d559
chuangcaleb <balechuang@gmail.com>
2023-04-21 02:46:26 +0800

    perf: improve lighthouse with aria-label icon button
    Cloudinary optimization haih

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Chuang Caleb";
-export const SITE_DESCRIPTION = "Caleb's personal website!";
+export const SITE_TITLE = "Chuang Caleb's Portfolio";
+export const SITE_DESCRIPTION =
+  "Chuang Caleb is a Software Engineer based in KL, Malaysia, and specializing in Front-End Web.";
