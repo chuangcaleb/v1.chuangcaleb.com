718dd17172c213fe821963d0d5de8317ee0a3d33
chuangcaleb <balechuang@gmail.com>
2023-04-29 17:55:37 +0100

    refac: use LINKS typed global object

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,4 +1,35 @@
-export const LINK_LINKEDIN = "https://www.linkedin.com/in/chuangcaleb/";
-export const LINK_GITHUB = "https://github.com/chuangcaleb";
-export const LINK_CV =
-  "https://docs.google.com/document/d/1qohsYe0w-Mq5yqIdNaHPuOHvs2lYvnGySULgR2Baqwk/edit?usp=sharing";
+interface Link {
+  label: string;
+  ariaLabel?: string;
+  href: string;
+  icon?: string;
+}
+
+const LINKS = {
+  LINKEDIN: {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/chuangcaleb/",
+    ariaLabel: "LinkedIn profile of Chuang Caleb",
+    icon: "mdi:linkedin",
+  },
+  GITHUB: {
+    label: "GitHub",
+    href: "https://github.com/chuangcaleb",
+    ariaLabel: "GitHub profile of Chuang Caleb",
+    icon: "mdi:github",
+  },
+  RESUME: {
+    label: "CV/Résumé",
+    href: "https://docs.google.com/document/d/1qohsYe0w-Mq5yqIdNaHPuOHvs2lYvnGySULgR2Baqwk/edit?usp=sharing",
+    ariaLabel: "CV/Résumé of Chuang Caleb",
+    icon: "mdi:card-account-details-star",
+  },
+  EMAIL: {
+    label: "Email",
+    href: "balechuang@gmail.com",
+    ariaLabel: "Email address of Chuang Caleb",
+    icon: "mdi:email-outline",
+  },
+} as const satisfies { [s: string]: Link };
+
+export default LINKS;
