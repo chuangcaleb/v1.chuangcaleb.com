8330605ae44a5e0ceab75c8c56ccdde69cb36184
chuangcaleb <balechuang@gmail.com>
2023-05-01 17:25:36 +0100

    feat: add the other tech card emojis

--- src/data/techStack.ts
+++ src/data/techStack.ts
@@ -9,44 +9,74 @@ interface Tech {
   // label: string;
   icon: string;
   stage: Stage;
+  isInterested: boolean;
+  isWork: boolean;
+  isFresh: boolean | null;
 }
 
 export const STACK = {
   Javascript: {
     icon: "logos:javascript",
     stage: "confident",
+    isInterested: true,
+    isWork: true,
+    isFresh: true,
   },
   Typescript: {
     icon: "logos:typescript-icon",
     stage: "confident",
+    isInterested: true,
+    isWork: true,
+    isFresh: true,
   },
   Python: {
     icon: "logos:python",
     stage: "confident",
+    isInterested: true,
+    isWork: false,
+    isFresh: false,
   },
   React: {
     icon: "logos:react",
     stage: "confident",
+    isInterested: true,
+    isWork: true,
+    isFresh: true,
   },
   Astro: {
     icon: "astro",
     stage: "confident",
+    isInterested: true,
+    isWork: false,
+    isFresh: true,
   },
   Next: {
     icon: "logos:nextjs-icon",
     stage: "learning",
+    isInterested: true,
+    isWork: true,
+    isFresh: true,
   },
   Django: {
     icon: "logos:django-icon",
     stage: "confident",
+    isInterested: false,
+    isWork: false,
+    isFresh: false,
   },
   "Material UI": {
     icon: "logos:material-ui",
     stage: "confident",
+    isInterested: false,
+    isWork: true,
+    isFresh: true,
   },
   Tailwind: {
     icon: "logos:tailwindcss-icon",
     stage: "expert",
+    isInterested: true,
+    isWork: true,
+    isFresh: true,
   },
 } as const satisfies { [s: string]: Tech };
 
