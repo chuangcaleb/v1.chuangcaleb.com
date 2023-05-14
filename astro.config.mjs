4ef6aa1d99c5dac086c08b4d44f77cbcdcddc975
chuangcaleb <balechuang@gmail.com>
2023-05-15 00:15:47 +0800

    feat: integrate contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://example.com",
   integrations: [
     mdx(),
