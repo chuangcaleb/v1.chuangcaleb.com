79ab6fb1acfc28db2b474e36feab34603668b81e
chuangcaleb <balechuang@gmail.com>
2023-06-01 20:29:36 +0800

    ui: simplify hero section layout
    don't watermark image under text

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://chuangcaleb.vercel.app",
+  compressHTML: true,
   integrations: [
     mdx(),
     tailwind({
